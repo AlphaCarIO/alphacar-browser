acb5c80d3c9d04ad76e452ce6397cb7d4655cd01
leochen <leo@leochens-MacBook-Pro.local>
2018-09-11 16:07:27 +0800

    Changed by leo

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,6 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_URL: "127.0.0.1:3000",
-  IPFS_URL: 'http://127.0.0.1:8080'
+  BASE_URL: '"127.0.0.1:3000"',
+  IPFS_URL: '"http://127.0.0.1:8080"'
 })
